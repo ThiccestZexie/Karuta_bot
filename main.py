ab4fd6c8f9571cb87cab985321a5e3d51abff103
Daniel Alchasov <danielalchasov03@gmail.com>
2023-10-26 01:09:30 +0200

    ehhh

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 
 import discord
 from discord.ext import commands
+from discord.ext.commands import Bot
 import random
 import os
+import requests
+import json
 from dotenv import load_dotenv
-
-
 
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
@@ -78,6 +79,15 @@ async def on_message(ctx):
             user = await bot.fetch_user(user_id)
             print(f" {user} has the id: {user_id}")
 
+    if "€funny"  in ctx.content.lower():
+        f = r"https://official-joke-api.appspot.com/random_joke"
+        a = jokes(f)
+        print("Sending joke...")
+
+        await ctx.channel.send(a["setup"])
+        await ctx.channel.send(a["punchline"])
+
+
     if "€check" in ctx.content.lower():
         if ctx.author.id in user_ids:
             await ctx.channel.send("You are in the list!")
@@ -87,16 +97,32 @@ async def on_message(ctx):
         for user_id in user_ids:
             user = await bot.fetch_user(user_id)
             print(f" {user} has the id: {user_id}")
+    if "x)" in ctx.content.lower():
+        user = await bot.fetch_user(ctx.author.id)
+        await ctx.channel.send("xD* " + user.mention)
+    if "€help" in ctx.content.lower():
+        embed = discord.Embed(title="Sample Embed", url="https://realdrewdata.medium.com/",
+                              description="This is an embed that will show how to build an embed and the different components",
+                              color=0xFF5733)
+        await ctx.channel.send(embed=embed)
     else:
         return
 
+@bot.command("add", help="Adds your ID to the list of people to DM when a server drop happens")
+async def add(ctx): 
+    if "€add" in ctx.content.lower():
+        print("Adding ID...")
+        if ctx.author.id in user_ids:
+            print("ID already in list!")
+            return
+        if ctx.author.id not in user_ids:
+            with open('ids.txt', 'a') as file:
+                file.write(str(ctx.author.id) + "\n")
+        user_ids.append(ctx.author.id)
 
-@commands.command()
-async def getId(ctx):
-    print("Sending ID...")
-    await ctx.channel.send(ctx.author.id)
-
-
+        user = await bot.fetch_user(ctx.author.id)
+        print(f" {user} has the id: {ctx.author.id}")
+        await ctx.channel.send("You have been added to the list!")
 @bot.event
 async def send_dm(ctx, *, message):
     for user_id in user_ids:
@@ -108,9 +134,17 @@ async def send_dm(ctx, *, message):
             print(f'Failed to send DM to {user.name} ({user.id}): {str(e)}')
 
 
-@bot.event
+@bot.command()
 async def shutdown(ctx):
     print("yatta")
     await ctx.bot.close()
+
+
+def jokes(f):
+    data = requests.get(f)
+    tt = json.loads(data.text)
+    return tt
+
+
 load_dotenv()
 bot.run(os.getenv("TOKEN"))
